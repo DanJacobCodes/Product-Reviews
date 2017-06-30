b53d7b75cfd51ee25d02b335c2040626d17f94e1
Dan Jacob <dansamueljacob@gmail.com>
2017-06-30 11:16:44 -0700

    add faker to seed db

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ group :development, :test do
   gem 'launchy'
   gem 'pry'
   gem 'shoulda-matchers'
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+15.times do |index|
+  Product.create!(name: Faker::Food.ingredient)
+end
+
+Review.destroy_all
+
+20.times do |index|
+  Review.create!(author: Faker::HowIMetYourMother.character,
+                content_body: Faker::Lorem.words(5),
+                rating: Faker::Number.between(1,5))
+end
