628c8e59f651939dab1d18a45d34d78b1f7885be
Dan Jacob <dansamueljacob@gmail.com>
2017-06-30 19:49:46 -0700

    add validation to limit rating to be equal to or less than 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, :presence => true
   validates :content_body, {:presence => true, :length => {:within => 50..250}}
-  validates :rating, :presence => true
+  validates :rating, {:presence => true, numericality: {greater_than: 0, less_than_or_equal_to: 5 }}
 end
